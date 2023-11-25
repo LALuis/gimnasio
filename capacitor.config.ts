24bf84adc8b7082b97ae7763909625764480cbe4
Luis Lopez <luis.lopez@splice.com>
2023-11-25 16:41:21 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Disfrutando del proceso FIT',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
